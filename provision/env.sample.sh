bbaa52c56c1361e06fffbf132af2a84dd46ba149
roelofjansen <roelof@rena-it.nl>
2018-04-09 22:38:23 +0200

    Update env.sample.sh
    fix typo ip

--- provision/env.sample.sh
+++ provision/env.sample.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export GOVC_INSECURE=1
-export GOVC_URL=<hostname/ipi esxi host>
+export GOVC_URL=<hostname/ip esxi host>
 export GOVC_USERNAME=root
 export GOVC_PASSWORD=<password>
 export GOVC_DATASTORE=<datastore name>
