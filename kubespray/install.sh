56beb1c3ae5e651a713be51dc2d429931a5dce48
Roelof Jansen <roelof@rena-it.nl>
2018-04-10 00:05:29 +0200

    update kubespray

--- kubespray/install.sh
+++ kubespray/install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm --name kubespray -v ~/github/renait/kubernetes/kubespray/inventory:/kubespray/inventory kubespray ansible-playbook -u core -i  ./inventory/explorer/hosts.ini cluster.yml -b -v --private-key=./inventory/explorer/.ssh/id_rsa
+docker run --rm --name kubespray -v ~/github/kubespray/kubespray/inventory:/kubespray/inventory kubespray ansible-playbook -i  ./inventory/explorer/hosts.ini cluster.yml -b -v --private-key=./inventory/explorer/.ssh/id_rsa
