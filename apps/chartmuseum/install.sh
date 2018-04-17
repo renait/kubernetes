203b2d63b9bf4c5d2e96c342bae852c448e3559c
Roelof Jansen <roelof@rena-it.nl>
2018-04-17 22:38:27 +0200

    k8s-cluster kubespray, owncloud helm chart 1.0.6, chartmuseum setup

--- apps/chartmuseum/install.sh
+++ apps/chartmuseum/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 kubectl apply -f pv.yaml
 kubectl apply -f pvc.yaml
-helm install -n chartmuseum -f values.yaml stable/chartmuseum
+helm upgrade --install -f values.yaml chartmuseum stable/chartmuseum
 
