5ddc26634a25a325be12a794a34e94b6668e5184
Roelof Jansen <roelof@rena-it.nl>
2018-04-19 23:31:21 +0200

    new chart: nexus, setup helm plugin 'push'

---
+++ apps/chartmuseum/setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+helm plugin install https://github.com/chartmuseum/helm-push
---
+++ apps/nexus/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+kubectl apply -f pv.yaml
+kubectl apply -f pvc.yaml
+helm upgrade --install -f values.yaml nexus stable/sonatype-nexus
+
