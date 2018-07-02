b32ea9b8a8e3d3dd0d11f368a799034047e94d52
roelofjansen <roelof@rena-it.nl>
2018-07-02 17:35:02 +0200

    Update uninstall.sh

--- apps/nextcloud/app/uninstall.sh
+++ apps/nextcloud/app/uninstall.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 helm delete --purge nextcloud
+kubectl delete pvc nextcloud-mariadb
+kubectl delete pvc nextcloud-data
+kubectl delete pvc nextcloud-html
+kubectl delete pv nextcloud-mariadb
+kubectl delete pv nextcloud-data
+kubectl delete pv nextcloud-html
