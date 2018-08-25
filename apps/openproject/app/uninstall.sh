c46af54649567d398944210999eb1a23e8b3d7e6
Roelof Jansen <roelof@rena-it.nl>
2018-08-25 03:06:01 +0200

    embed postgresql chart in openproject chart as a requirement

--- apps/openproject/app/uninstall.sh
+++ apps/openproject/app/uninstall.sh
@@ -2,5 +2,7 @@
 helm delete --purge openproject
 kubectl delete pvc openproject-logs
 kubectl delete pvc openproject-data
+kubectl delete pvc openproject-pgsql
 kubectl delete pv openproject-logs
 kubectl delete pv openproject-data
+kubectl delete pv openproject-pgsql
