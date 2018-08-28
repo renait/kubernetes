ee2006d110abc88dcb5cf426d14da91a5376b853
Roelof Jansen <roelof@rena-it.nl>
2018-08-28 23:23:39 +0200

    persistent grafana prometheus

--- monitoring/kube-prometheus/install.sh
+++ monitoring/kube-prometheus/install.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+kubectl apply -f dashboards.yaml
 helm upgrade --install kube-prometheus coreos/kube-prometheus -f values.yaml --namespace monitoring
+kubectl apply -f pv.yaml
+#kubectl apply -f pvc.yaml
--- monitoring/kube-prometheus/uninstall.sh
+++ monitoring/kube-prometheus/uninstall.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 helm delete --purge kube-prometheus
+kubectl delete pvc -n monitoring kube-prometheus-grafana
+kubectl delete pvc -n monitoring prometheus-kube-prometheus-db-prometheus-kube-prometheus-0
+kubectl delete pvc -n monitoring alertmanager-kube-prometheus-db-alertmanager-kube-prometheus-0
+kubectl delete pv -n monitoring kube-prometheus-grafana
+kubectl delete pv -n monitoring kube-prometheus-db-prometheus
+kubectl delete pv -n monitoring kube-prometheus-db-alertmanager
+kubectl delete configmap -n monitoring custom-dashboards
