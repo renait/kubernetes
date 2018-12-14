b985a9eeb731b411376c22273180edd652b0fabe
Roelof Jansen <roelof@rena-it.nl>
2018-12-14 23:47:40 +0100

    Serviio configurable pv config

--- apps/serviio/install.sh
+++ apps/serviio/install.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#kubectl apply -f pv-config.yaml
 helm upgrade --install -f values.yaml serviio explorer/serviio
--- apps/serviio/uninstall.sh
+++ apps/serviio/uninstall.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 helm delete --purge serviio
-#kubectl delete pvc serviio-config
-#kubectl delete pv serviio-config
