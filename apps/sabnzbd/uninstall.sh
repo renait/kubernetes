664356226b345b85b0d0b2bd04d6e661518f1ee1
Roelof Jansen <roelof@rena-it.nl>
2018-12-14 18:13:17 +0100

    Made sabnzbd persistent volume configurable

--- apps/sabnzbd/uninstall.sh
+++ apps/sabnzbd/uninstall.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 helm delete --purge sabnzbd
-kubectl delete pvc sabnzbd-config
-kubectl delete pv sabnzbd-config
-kubectl delete pvc sabnzbd-download
-kubectl delete pv sabnzbd-download
