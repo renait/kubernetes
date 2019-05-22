a8450346e01c01c863fa1c81d3c6c264dee381fe
Roelof Jansen <roelof@rena-it.nl>
2019-05-22 12:51:39 +0200

    updated with nginx config. changed persistence

--- apps/nextcloud/app/uninstall.sh
+++ apps/nextcloud/app/uninstall.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 helm delete --purge nextcloud
 kubectl delete pvc nextcloud-data
-kubectl delete pvc nextcloud-apache
 kubectl delete pvc data-nextcloud-mariadb-0
 kubectl delete pv nextcloud-data
-kubectl delete pv nextcloud-apache
 kubectl delete pv data-nextcloud-mariadb-0
