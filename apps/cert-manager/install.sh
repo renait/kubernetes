601e047ce627a421e29791e359949b3b39fc7f62
Roelof Jansen <roelof@rena-it.nl>
2019-10-02 17:43:17 +0200

    updated some charts

--- apps/cert-manager/install.sh
+++ apps/cert-manager/install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 kubectl apply \
-    -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
+    -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.9/deploy/manifests/00-crds.yaml
 
 kubectl label namespace cert-manager certmanager.k8s.io/disable-validation="true" --overwrite=true
 
 helm upgrade --install cert-manager jetstack/cert-manager -f values.yaml --namespace cert-manager
 
 kubectl create secret tls ca-kubernetes-secret --cert=ca-kubernetes.cert.pem --key=ca-kubernetes.key.pem --namespace default
-kubectl -n default create -f issuer-kubernetes.yaml
-
 kubectl create secret tls ca-kubernetes-secret --cert=ca-kubernetes.cert.pem --key=ca-kubernetes.key.pem --namespace kube-system
-kubectl -n kube-system create -f issuer-kubernetes.yaml
-
 kubectl create secret tls ca-kubernetes-secret --cert=ca-kubernetes.cert.pem --key=ca-kubernetes.key.pem --namespace monitoring
+
+sleep 30
+
 kubectl -n monitoring create -f issuer-kubernetes.yaml
+kubectl -n default create -f issuer-kubernetes.yaml
+kubectl -n kube-system create -f issuer-kubernetes.yaml
--- apps/sonarr/install.sh
+++ apps/sonarr/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kubectl apply -f pv-config.yaml
-helm upgrade --install sonarr explorer/sonarr
+helm upgrade --install -f values.yaml sonarr explorer/sonarr
