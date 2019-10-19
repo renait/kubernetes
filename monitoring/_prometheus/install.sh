e34ed76653ce2e3f2dea21d26d10cc988bd03fdf
Roelof Jansen <roelof@rena-it.nl>
2019-10-19 16:15:30 +0200

    [] Update openproject chart to use bitname postgres

--- monitoring/_prometheus/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-helm install coreos/prometheus-operator --name prometheus-operator --namespace monitoring
-helm install coreos/kube-prometheus --name kube-prometheus --set global.rbacEnable=true --namespace monitoring
