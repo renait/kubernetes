8e7ff73e96d12408d848c1f374870128951dd1b5
Roelof Jansen <roelof@rena-it.nl>
2019-10-19 11:58:08 +0200

    minor updates

--- apps/k8s-dashboard/install.sh
+++ apps/k8s-dashboard/install.sh
@@ -2,4 +2,5 @@
 kubectl -n kube-system apply -f serviceaccount.yaml
 kubectl -n kube-system apply -f clusterrolebinding.yaml
 
-helm upgrade --namespace kube-system --install -f values.yaml k8s-dashboard stable/kubernetes-dashboard
+#helm upgrade --namespace kube-system --install -f values.yaml k8s-dashboard stable/kubernetes-dashboard --version 1.8.0
+helm upgrade --namespace kube-system --install -f values.yaml k8s-dashboard stable/kubernetes-dashboard --version 1.10.0
--- monitoring/prometheus-operator/install.sh
+++ monitoring/prometheus-operator/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #kubectl apply -f dashboards.yaml
-helm upgrade --install prometheus-operator stable/prometheus-operator -f custom-values.yaml --namespace monitoring
+#helm upgrade --install prometheus-operator stable/prometheus-operator -f values.yaml --namespace monitoring --version 6.4.0
+helm upgrade --install prometheus-operator stable/prometheus-operator -f values.yaml --namespace monitoring --version 6.20.1
 kubectl apply -f pv.yaml
