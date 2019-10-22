9a2d687d35f6dd250a4bb8f13bb5b410d03b05bd
Roelof Jansen <roelof@rena-it.nl>
2019-10-22 13:28:59 +0200

    fix cert-manager scripts

--- apps/cert-manager/install.sh
+++ apps/cert-manager/install.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
+
 kubectl apply \
-    -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.9/deploy/manifests/00-crds.yaml
-    #-f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
+    -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.10/deploy/manifests/00-crds.yaml
 
-#kubectl label namespace cert-manager certmanager.k8s.io/disable-validation="true" --overwrite=true
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation="true" --overwrite=true
 
-helm upgrade --install cert-manager jetstack/cert-manager -f values.yaml --namespace cert-manager --version v0.9.1
-#helm upgrade --install cert-manager jetstack/cert-manager -f values.yaml --namespace cert-manager --version v0.11.0
+helm upgrade --install cert-manager jetstack/cert-manager -f values.yaml --namespace cert-manager --version v0.10.1
 
 kubectl create secret tls ca-kubernetes-secret --cert=ca-kubernetes.cert.pem --key=ca-kubernetes.key.pem --namespace default
 kubectl create secret tls ca-kubernetes-secret --cert=ca-kubernetes.cert.pem --key=ca-kubernetes.key.pem --namespace kube-system
 kubectl create secret tls ca-kubernetes-secret --cert=ca-kubernetes.cert.pem --key=ca-kubernetes.key.pem --namespace monitoring
 
-sleep 30
+sleep 5
 
-kubectl -n monitoring create -f issuer-kubernetes.yaml
-kubectl -n default create -f issuer-kubernetes.yaml
-kubectl -n kube-system create -f issuer-kubernetes.yaml
+kubectl -n monitoring apply -f issuer-kubernetes.yaml
+kubectl -n default apply -f issuer-kubernetes.yaml
+kubectl -n kube-system apply -f issuer-kubernetes.yaml
--- apps/cert-manager/uninstall.sh
+++ apps/cert-manager/uninstall.sh
@@ -5,3 +5,7 @@ kubectl delete secret ca-kubernetes-secret -n kube-system
 kubectl delete secret ca-kubernetes-secret -n monitoring
 kubectl delete secret ca-kubernetes-secret -n default
 
+kubectl delete issuer ca-kubernetes-issuer -n kube-system
+kubectl delete issuer ca-kubernetes-issuer -n monitoring
+kubectl delete issuer ca-kubernetes-issuer -n default
+
