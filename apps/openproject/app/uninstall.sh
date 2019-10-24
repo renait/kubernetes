2dd8ab2ad846828c348bfc320684706419a06168
Roelof Jansen <roelof.jansen@minbzk.nl>
2019-10-24 16:56:59 +0200

    [] Openproject fixup chart

--- apps/openproject/app/uninstall.sh
+++ apps/openproject/app/uninstall.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 helm delete --purge openproject
-kubectl delete pvc openproject-logs
 kubectl delete pvc openproject-data
 kubectl delete pvc openproject-pgsql
-kubectl delete pv openproject-logs
 kubectl delete pv openproject-data
 kubectl delete pv openproject-pgsql
