b2192d0084b8ed12cbb2df23f94f2d0243a2a591
Roelof Jansen <roelof.jansen@minbzk.nl>
2019-10-28 10:34:49 +0100

    [] Update gitlab chart

--- apps/gitlab/uninstall.sh
+++ apps/gitlab/uninstall.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 helm delete --purge gitlab
+kubectl delete pvc gitlab-config
+kubectl delete pvc gitlab-logs
 kubectl delete pvc gitlab-data
 kubectl delete pvc gitlab-pgsql
 kubectl delete pvc gitlab-redis
 kubectl delete pv gitlab-data
+kubectl delete pv gitlab-logs
+kubectl delete pv gitlab-config
 kubectl delete pv gitlab-pgsql
 kubectl delete pv gitlab-redis
